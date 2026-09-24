f52bd62ce5160bae5582a5fd77b2b54d55942e96
iibrahimboim <ytjazs04@gmail.com>
2026-09-24 17:06:20 +0700

    Merapikan layout dengan widget ListView()

--- lib/main.dart
+++ lib/main.dart
@@ -167,6 +167,15 @@ class _HomePageState extends State<HomePage> {
                     trailing: Text("50", style: TextStyle(color: Colors.green)),
                   ),
                 ),
+               Card(
+                  elevation: 20,
+                  child: ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("Boim"),
+                    subtitle: Text("Kamuu lagi apa bus"),
+                    trailing: Text("50", style: TextStyle(color: Colors.green)),
+                  ),
+                ),
                 Card(
                   elevation: 20,
                   child: ListTile(
